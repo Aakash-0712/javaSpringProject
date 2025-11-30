51ca29513d3573fd517de451a0fa0a0aa3c40e35
Aakash Rathva <=>
2025-11-30 21:15:35 +0530

    projects updated also add student pojo class and jsp for register

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/SpringMvcApplication/WEB-INF/views/student-success.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h2>Registration Successful</h2>
+
+<p>Name: ${data.name}</p>
+<p>Email: ${data.email}</p>
+<p>Course: ${data.course}</p>
+
+</body>
+</html>
---
+++ SpringMvcApplication/src/main/java/com/springmvc/Student.java
@@ -0,0 +1,31 @@
+package com.springmvc;
+
+
+public class Student {
+	private String name;
+	private String course;
+	private String email;
+	
+	//Getter and Setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+}
